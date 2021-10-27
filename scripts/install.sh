9f495c4a8163e2bc601d129fa7f964d0d5a7e3b1
Marcel <marcel.bernet@ch-open.ch>
2021-10-27 18:39:37 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@
 sudo add-apt-repository ppa:gns3/ppa -y
 sudo apt-update
 export DEBIAN_FRONTEND=noninteractive
-sudo apt install gns3-server gns3-gui  -y
+sudo apt install gns3-gui  -y
 
 # Introseite (= README dieses Repository)
 bash -x /opt/lernmaas/helper/intro
