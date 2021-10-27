64865a905ec74efb346908f22c13b62edc8fe1ea
Marcel <marcel.bernet@ch-open.ch>
2021-10-27 18:18:29 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@
 
 sudo add-apt-repository ppa:gns3/ppa -y
 sudo apt-update
-sudo apt install gns3-gui gns3-server gns3-gui -y
+sudo apt install gns3-gui -y
 
 # Introseite (= README dieses Repository)
 bash -x /opt/lernmaas/helper/intro
