0040dcf4e736834dfbaf545929709f9916447055
Marcel <marcel.bernet@ch-open.ch>
2023-04-07 11:35:13 +0200

    ohne KVM

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@
 
 cd /tmp
 curl https://raw.githubusercontent.com/GNS3/gns3-server/master/scripts/remote-install.sh > gns3-remote-install.sh
-sudo bash gns3-remote-install.sh 
+sudo bash gns3-remote-install.sh --without-kvm
 
 # Introseite 
 bash -x /opt/lernmaas/helper/intro
