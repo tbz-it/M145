e2dec815571c70dc72da034a224ad51f56f437b8
Marcel <marcel.bernet@ch-open.ch>
2023-04-07 10:50:47 +0200

    intro

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,3 +6,5 @@ cd /tmp
 curl https://raw.githubusercontent.com/GNS3/gns3-server/master/scripts/remote-install.sh > gns3-remote-install.sh
 sudo bash gns3-remote-install.sh 
 
+# Introseite 
+bash -x /opt/lernmaas/helper/intro
