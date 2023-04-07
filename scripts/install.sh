ec2e176b715ca7d145db6648b25da7f55c57d748
Marcel <marcel.bernet@ch-open.ch>
2023-04-07 13:43:27 +0200

    intro

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,9 +2,10 @@
 #
 #	Installationsscript Modul 145 - Netzwerk betreiben und erweitern
 
+# Introseite 
+bash -x /opt/lernmaas/helper/intro
+
+# GNS3 Labor
 cd /tmp
 curl https://raw.githubusercontent.com/GNS3/gns3-server/master/scripts/remote-install.sh > gns3-remote-install.sh
 sudo bash gns3-remote-install.sh --without-kvm
-
-# Introseite 
-bash -x /opt/lernmaas/helper/intro
