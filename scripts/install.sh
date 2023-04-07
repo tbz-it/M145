e7fc8d53316343f050ceb65a7dd41aa1f3437cfb
Marcel <marcel.bernet@ch-open.ch>
2023-04-07 14:49:15 +0200

    kvm rein

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,4 +8,4 @@ bash -x /opt/lernmaas/helper/intro
 # GNS3 Labor
 cd /tmp
 curl https://raw.githubusercontent.com/GNS3/gns3-server/master/scripts/remote-install.sh > gns3-remote-install.sh
-sudo bash gns3-remote-install.sh --without-kvm
+sudo bash gns3-remote-install.sh
